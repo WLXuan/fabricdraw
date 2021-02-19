20f460b50834973a859860aa03074880b3f56721
齐泳睿 <qiyongrui@192.168.1.48>
2021-02-19 11:56:52 +0800

    add download function in vue

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import BootstrapVue from 'bootstrap-vue'
 import fabric from 'fabric'
-Vue.use(fabric);
+Vue.use(VueAxios, axios)
+Vue.prototype.$axios = axios
+Vue.use(fabric)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
