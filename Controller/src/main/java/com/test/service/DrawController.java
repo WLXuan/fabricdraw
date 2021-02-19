46153750fd796a7a1231cd48006ddd5a8f20afd9
齐泳睿 <qiyongrui@192.168.1.48>
2021-02-19 20:55:31 +0800

    add the starter of controller

--- Controller/src/main/java/com/test/service/DrawController.java
+++ Controller/src/main/java/com/test/service/DrawController.java
@@ -1,5 +1,6 @@
 package com.test.service;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import org.springframework.expression.spel.ast.NullLiteral;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,6 +8,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.File;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:8080")
@@ -14,6 +22,20 @@ import java.util.Map;
 public class DrawController {
     @PostMapping("/download")
     public ResponseEntity<byte[]> download(@RequestBody Map<String, Object> args){
-        return null;
+        JsonReader orgsReader = Json.createReader(new StringReader((String) args.get("jsonOrgs")));
+        JsonReader channelsReader = Json.createReader(new StringReader((String) args.get("jsonChannels")));
+        JsonObject orgJson = orgsReader.readObject();
+        JsonObject chnJson = channelsReader.readObject();
+        String[] tempPath = DrawController.class.getResource("/").getPath().split("/");
+        StringBuilder BASE = new StringBuilder();
+        for(int i=0; i<tempPath.length-4; i++) {
+            BASE.append("/").append(tempPath[i]);
+        }
+        BASE.append("/resources/templates/");
+
+        List<String> fileList = new ArrayList<String>();
+        fileList.add(BASE + "configtx.yaml");
+        fileList.add(BASE + "crypto-config.yaml");
+
     }
 }
